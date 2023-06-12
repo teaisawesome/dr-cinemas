04b0c87c477b3304e9efe8a6c212602ea9233b27
teaisawesome <dekany.r123@gmail.com>
2023-06-12 22:36:45 +0200

    server default port config by env

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const app = createApp(App)
 
