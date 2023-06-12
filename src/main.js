9495886a9980e56c0fad123ae5655f71bc9499f9
teaisawesome <dekany.r123@gmail.com>
2023-06-12 23:31:41 +0200

    main.js refact > axios base url comes from .env variable

--- src/main.js
+++ src/main.js
@@ -5,14 +5,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
-import dotenv from 'dotenv'
-
-dotenv.config();
 
 const app = createApp(App)
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: process.env.VUE_APP_AXIOS_BASE_URL
 })
 
 app.config.globalProperties.$axios = axiosInstance
