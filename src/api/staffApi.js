7031a70b157dd1756650c5895d9514a1a0f0e23f
teaisawesome <dekany.r123@gmail.com>
2023-07-17 20:54:27 +0200

    staff api: get staff - find all staff endpoint added

--- src/api/staffApi.js
+++ src/api/staffApi.js
@@ -1,5 +1,7 @@
 module.exports = function(app) {
     const staffController = require('../controllers/staffController')
 
+    app.get('/staff', staffController.findAllStaff)
     app.post('/staff', staffController.createStaff)
+
 }
