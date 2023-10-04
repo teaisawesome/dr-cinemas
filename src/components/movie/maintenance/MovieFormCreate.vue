55c7a60863f04d1135234e83e5104fc6ff709c18
teaisawesome <dekany.r123@gmail.com>
2023-10-04 23:58:02 +0200

    MovieFormCreate refact

--- src/components/movie/maintenance/MovieFormCreate.vue
+++ src/components/movie/maintenance/MovieFormCreate.vue
@@ -3,19 +3,21 @@
         <form @submit.prevent="submitForm">
             <label class="form-label movie-form-label">Főcím</label>
             <input class="form-control" type="text" placeholder="...">
-            <label class="form-label mt-3 movie-form-label">Műfajok</label>
+            <label class="form-label movie-form-label">Műfajok</label>
             <select multiple class="form-control genre-multiselect" id="exampleFormControlSelect2">
                 <option class="movie-genre-option" v-for="(movieGenre, index) in getSortedMovieGenres" :key="index" :value="index + 1">
                     {{ movieGenre.genreName }}
                 </option>
             </select>
-            <label class="form-label mt-3 movie-form-label">Megjelenés</label>
+            <label class="form-label movie-form-label">Rendező(k)</label>
+            <input class="form-control" type="text" placeholder="...">
+            <label class="form-label movie-form-label">Megjelenés</label>
             <input class="form-control" type="date">
-            <label class="form-label mt-3 movie-form-label">Hossz (perc)</label>
+            <label class="form-label movie-form-label">Hossz (perc)</label>
             <input class="form-control" type="number" placeholder="...">
-            <label class="form-label mt-3 movie-form-label">Korhatár</label>
+            <label class="form-label movie-form-label">Korhatár</label>
             <input class="form-control" type="number" placeholder="...">
-            <label class="form-label mt-3 movie-form-label">Leírás</label>
+            <label class="form-label movie-form-label">Leírás</label>
             <textarea class="form-control" rows="3" placeholder="..."></textarea>
             <button class="btn mt-3 movie-save-btn">MENTÉS</button>
         </form>
@@ -63,6 +65,7 @@ export default {
     color: goldenrod;
     text-transform: uppercase;
     padding-left: 0.5rem;
+    margin-top: 0.5rem;
 }
 .movie-genre-option:checked {
     background-color: goldenrod;
